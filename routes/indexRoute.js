583fb3c74d775657fd1ed51e73fd20280f324418
Raihan Sharif <raihansharif92@gmail.com>
2025-11-23 20:58:50 +0000

    use prisma session store

---
+++ routes/indexRoute.js
@@ -0,0 +1,19 @@
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import prisma from "../db/prismaClient";
+
+const mountRoutes = (app) => {
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET,
+            resave: false,
+            saveUninitialized: false,
+            store: new PrismaSessionStore(prisma, {
+                checkedPeriod: 2 * 60 * 1000,
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined,
+            }),
+            cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+        })
+    );
+};
