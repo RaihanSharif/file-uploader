461d2ab909d6b8a640b4ac98534fefdf342ff6d9
Raihan Sharif <raihansharif92@gmail.com>
2025-12-17 22:21:09 +0000

    finish userQueries

--- prisma/queries/userQueries.js
+++ prisma/queries/userQueries.js
@@ -4,15 +4,46 @@ import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-async function getEmail(email) {}
+async function getUserByEmail(email) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    });
+    return user;
+}
 
-async function getUsername(id) {}
+async function getUserByUsername(username) {
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username,
+        },
+    });
+    return user;
+}
 
-async function getUser(id) {}
+async function getUserById(id) {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: id,
+        },
+    });
+    return user;
+}
 
 async function addUser(username, email, password) {
     const hashedPW = await bcrypt.hash(password, 12);
-    // insert into User table using prisma
+    try {
+        prisma.user.create({
+            data: {
+                username: username,
+                email,
+                password: hashedPW,
+            },
+        });
+    } catch (err) {
+        console.err(err);
+    }
 }
 
 export { getEmail, getUsername, getUser, addUser };
